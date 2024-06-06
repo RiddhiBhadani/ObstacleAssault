3b394b5ba876eacf520cae9a18558e20758dad70
Riddhi Patel <39990641+RiddhiBhadani@users.noreply.github.com>
2024-06-06 11:38:42 -0400

    Added -  Moving Platform C++ Class

---
+++ Source/ObstacleAssault/MovingPlatform.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class OBSTACLEASSAULT_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+UPROPERTY(EditAnyWhere)
+	int32 Int1 = 0;
+UPROPERTY(EditAnyWhere)
+	int32 Int2 = 0;
+UPROPERTY(EditAnyWhere)
+	int32 Int3 = 0;
+	UPROPERTY(EditAnyWhere)
+	int32 Int4 = 0;
+};
